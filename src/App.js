3001e3a20a49af50c805d32695e34ee2d2332403
Ali Reubenstone <alison.reubenstone@flaitronschool.com>
2019-07-31 13:16:52 -0400

    Add components folder w Splash, LogIn, SignUp

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+import Splash from './components/Splash'
+
+function App() {
+  return (
+    <div>
+    <Splash />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Splash.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+class Splash extends React.Component {
+
+  render () {
+    return (
+      <div>
+        <h1>I'm the Splash Page</h1>
+        <p>Who We Are / What We Do / How It Works</p>
+        <ul>
+          <li>Input your 2 most recent jobs including your current one.</li>
+          <li>Input your 5 top skills that most directly relate to the job that you want.</li>
+          <li>We analyze the jobs that are ACTIVELY HIRING that you will most likely receive interviews for.</li>
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default Splash
